8ec7a85a09dddb30fa0be22f6ffe479fab2bb6eb
TekniqueJoseph <joejacksun@gmail.com>
2023-04-24 18:54:18 -0400

    formatted code

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-let myName = 'Joseph'
+// let myName = 'Joseph'
 
-if (myName === 'Joseph') {
-    let hobbies = ['Sports', 'Cooking']
-    console.log('if', hobbies)
+// if (myName === 'Joseph') {
+//     let hobbies = ['Sports', 'Cooking']
+//     console.log('if', hobbies)
 
-}
+// }
 
-function greet() {
-    let age = 52;
-    let myName = 'Buju'
-    console.log('func=>',myName, age, hobbies)
-}
+// function greet() {
+//     let age = 52;
+//     let myName = 'Buju'
+//     console.log('func=>',myName, age, hobbies)
+// }
 
-console.log('log=>', myName, hobbies)
+// console.log('log=>', myName, hobbies)
 
-greet()
+// greet()
